e183faf693c7e211e86b7561fb69e2797be81772
cedricragotarttherapie-dotcom <cedricragot.arttherapie@gmail.com>
2026-05-03 20:45:02 +0200

    fix deploy render + tailwind + supabase

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
+    "./index.html",
     "./client/index.html",
     "./client/src/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
